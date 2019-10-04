88b54f4d3ca4d214508dc465111707eeafb214b9
Trevor254 <trevoromondi811@gmail.com>
2019-10-04 10:51:00 +0300

    carried out destructuring in the ninjas.js file

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,9 +1,9 @@
 import React  from 'react';
 
 
-    const Ninjas = (props) => {    
-        const{ninjas}=props;
+    const Ninjas = ({ninjas}) => {    // we have ninjas of the props so we are destructuring directly inside the brackets where we receive the props
         const ninjaList = ninjas.map (ninja => {
+           // const {ninjas} = props;
             return (
                 <div className="ninja" key={ninja.id}>
                  <div>{ninja.name}</div>
