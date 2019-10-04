2582e9ae109c5692980b5d32cb0ab3afb7b17867
Trevor254 <trevoromondi811@gmail.com>
2019-10-04 11:14:05 +0300

    added an if statement that checks if age is greater than 20, to return those specific ninjas with an age greater than 20

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -4,15 +4,20 @@ import React  from 'react';
     const Ninjas = ({ninjas}) => {    // we have ninjas of the props so we are destructuring directly inside the brackets where we receive the props
         const ninjaList = ninjas.map (ninja => {
            // const {ninjas} = props;
-            return (
-                <div className="ninja" key={ninja.id}>
-                 <div>{ninja.name}</div>
-                 <div>{ninja.age}</div>
-                 <div>{ninja.belt}</div>
-                 
-                </div>
-            )
+           if(ninja.age > 20) {
+                return (
+                    <div className="ninja" key={ninja.id}>
+                    <div>{ninja.name}</div>
+                    <div>{ninja.age}</div>
+                    <div>{ninja.belt}</div>
+                    
+                    </div>
+                )
+           } else {
+               return null
+           }
         })
+    
     
         //console.log(this.props);
         return(
